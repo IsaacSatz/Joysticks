d7fe89206b3e76293f42fa11538b2a8be3c9dd7f
IsaacSatz <isaacsatz@gmail.com>
2013-10-19 15:26:33 -0700

    Update Joystick.h

--- Joystick.h
+++ Joystick.h
@@ -1,6 +1,9 @@
 #ifndef JOYSTICK_H
 #define JOYSTICK_H
 #include <WPIlib.h>
+#define PORT_JS_SPEED 1
+#define PORT_JS_TURN 2
+#define PORT_JS_OPERATOR 3
 class Joystick{
   private:
     Joystick speedStick;
