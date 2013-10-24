9576b921059837d2fd576901eff74365d9860516
Isaac Satz <isaacsatz@gmail.com>
2013-10-24 16:26:19 -0700

    Update Autonomous\UserControl.cpp

--- Autonomous\UserControl.cpp
+++ Autonomous\UserControl.cpp
@@ -33,6 +33,7 @@ void Autonomous(){
 //if need be can be changed (most likely will be changed)
 //the stuff we want the robot to do during autonomous mode
   //right
+  for(int i=0, i<16, i++){
   leftVic1.Set(0.5);
   leftVic2.Set(0.5);
   rightVic1.Set(0.0);
@@ -56,45 +57,16 @@ void Autonomous(){
   rightVic1.Set(0.0);
   rightVic2.Set(0.0);
   }
-};
-
-START_ROBOT_CLASS(MyRobot);
-void OperatorControl(){
-  while(isOperatorControl())
-    
-  if(speedStick.GetX() >= 0.2)
-    leftVic1.Set(0.5);
-    leftVic2.Set(0.5);
-    rightVic1.Set(-0.5);
-    rightVic2.set(-0.5);
-    Wait(.001);
-  if(speedsStick.GetX() <= -0.2)
-    //printf("1X: %f\n", speedStick.GetX());
-    leftVic1.Set(-0.5);
-    leftVic2.Set(-0.5);
-    rightVic1.Set(0.5);
-    rightVic.Set(0.5);
-    Wait(.001);
-  if (speedStick.GetX()>=-0.2 && speedStick.GetX() <= 0.2)
-    leftVic1.Set(0.0);
-    leftVic2.Set(0.0);
-    rightVic1.Set(0.0);
-    rightVic2.Set(0.0);
-    Wait(.001);
-  for(turnStick.GetY() >= 0.2 && leftVic1.Get() <= 1.5 && leftVic2.Get() <= 1.5)
-    leftVic1.Set(leftVic1.Get()+0.5);
-    leftVic2.Set(leftVic2.Get()+0.5);
-    Wait(.001);
-  for(turnStick.GetY() >= 0.2 && leftVic1.Get() >1.5 && leftVic2.Get() > 1.5)
-    rightVic1.Set(rightVic1.Get()+0.5);
-    rightVic2.Set(rightVic2.Get()+0.5);
-    Wait(.001);
-  for(turnStick.GetY() <= -0.2 && rightVic1.Get() >= -1.5 && rightVic2.Get() >= -1.5)
-    rightVic1.Set(rightVic1.Get()-0.5);
-    rightVic2.Set(rightVic2.Get()-0.5);
-    Wait(.001);
-  for(turnStick.GetY() > -0.2 && turnStick.GetY() < 0.2 && rightVic1.Get() >= -1.5 && rightVic2.Get() >= -1.5)
-      leftVic1.Set(leftVic1.Get()-0.5);
-      leftVic2.Set(leftVic2.Get()-0.5);
-      Wait(.001);
 }
+
+void OperatorControl(){
+  while(IsOperatorControl()){
+    
+  leftVic1.Set(speedStick.GetY+turnStick.GetX);
+  leftVic2.Set(speedStick.GetY+turnStick.GetX);
+  rightVic1.Set(-(speedStick.GetY-turnStick.GetX));
+  rightVic2.Set(-(speedStick.GetY-turnStick.GetX));
+  wait(0.005);l
+  }
+};
+START_ROBOT_CLASS(MyRobot);
