14426268bdc449fb4f9474910af62b2c01ac5585
Isaac Satz <isaacsatz@gmail.com>
2013-10-26 15:27:55 -0700

    fixed typo

--- IterativeRobotJoysticks.cpp
+++ IterativeRobotJoysticks.cpp
@@ -2,7 +2,7 @@
 #define PORT_DRIVE_VIC_1 5
 #define PORT_DRIVE_VIC_2 10
 #define PORT_DRIVE_VIC_3 2
-#defind PORT_DRIVE_VIC_4 1
+#define PORT_DRIVE_VIC_4 1
 #define PORT_JS_SPEED 1
 #define PORT_JS_TURN 2
 #define PORT_JS_CONTROL 3
