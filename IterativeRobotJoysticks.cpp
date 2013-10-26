8dc5f4e1c788e8e1c835057b3877f5ad9350c6e4
Isaac Satz <isaacsatz@gmail.com>
2013-10-26 15:23:09 -0700

    Create IterativeRobotJoysticks.cpp

---
+++ IterativeRobotJoysticks.cpp
@@ -0,0 +1,68 @@
+#include <WPILib>
+#define PORT_DRIVE_VIC_1 5
+#define PORT_DRIVE_VIC_2 10
+#define PORT_DRIVE_VIC_3 2
+#defind PORT_DRIVE_VIC_4 1
+#define PORT_JS_SPEED 1
+#define PORT_JS_TURN 2
+#define PORT_JS_CONTROL 3
+
+class MyRobot : public IterativeRobot{
+Victor leftVic1;
+Victor leftVic2;
+Victor rightVic1;
+Victor rightVic2;
+Joystick speedStick;
+Joystick turnStick;
+Joystick controlStick;
+Timer t;
+public:
+MyRobot():
+leftVic1(PORT_DRIVE_VIC_1),
+leftVic2(PORT_DRIVE_VIC_2),
+rightVic1(PORT_DRIVE_VIC_3),
+rightvic2(PORT_DRIVE_VIC_4),
+speedStick(PORT_JS_SPEED),
+turnStick(PORT_JS_TURN),
+controlStick(PORT_JS_CONTROL)
+{
+
+}
+void AutonomousInit{
+t.Start();
+}
+void AutonomousPeriodic(){
+  if(t.Get()<5.0){
+    leftVic1.Set(0.5);
+    leftVic2.Set(0.5);
+    rightVic1.Set(-0.5);
+    rightVic2.Set(-0.5);
+  }
+  else if(t.Get()<10.0){
+    leftVic1.Set(0.5);
+    leftVic2.Set(0.5);
+    rightVic1.Set(0.0);
+    rightVic2.Set(0.0);
+  }
+  else if(t.Get()<15.0){
+    leftVic1.Set(0.0);
+    leftVic2.Set(0.0);
+    rightVic1.Set(0.0);
+    rightvic2.Set(0.0);
+}
+void AutonomousDisabled(){
+  print("Autonomous mode disabled")
+}
+void TeleopInit(){
+  
+}
+void TeleopPeriodic(){
+  leftvic1.Set(speedStick.GetY+turnStick.GetX);
+  leftVic2.Set(speedStick.GetY+turnStick.GetX);
+  rightVic1.Set(-(speedStick.GetY-turnStick.GetX));
+  rightVic2.Set(-(speedStick.GetY-turnStick.GetX));
+}
+void TeleopDisabled(){
+print("Teleop Disabled");
+}
+START_ROBOT_CLASS(MyRobot);
