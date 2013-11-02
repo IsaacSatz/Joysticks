e8a639f00c63dcf25b5faa5e914a8566d20c6706
Isaac Satz <isaacsatz@gmail.com>
2013-11-02 14:47:55 -0700

    Update IterativeRobotJoysticks.cpp
    fixed backslashes

--- IterativeRobotJoysticks.cpp
+++ IterativeRobotJoysticks.cpp
@@ -93,7 +93,7 @@ class MyRobot : public IterativeRobot {
 			}
 		}
 		else if(shooterState==SPINNING){
-			printf("Charging up/n");
+			printf("Charging up\n");
 			shootTimer.Start();
 			shootVic1.Set(1.0);
 			shootVic2.Set(1.0);
@@ -113,7 +113,7 @@ class MyRobot : public IterativeRobot {
 			}
 		}
 		else if(shooterState==RECEDING){
-			printf("Retracting/n");
+			printf("Retracting\n");
 			recedeTimer.Start();
 			if(recedeTimer.Get()>2.0){
 				recedeTimer.Stop();
